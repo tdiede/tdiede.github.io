7f14547a38a03d5a3af2029be39a0be5fded4b91
tdiede <tdiede@gmail.com>
2016-09-18 21:35:26 -0700

    Added favicon.

--- static/js/site.js
+++ static/js/site.js
@@ -2,24 +2,34 @@
 
 
 var canvas = document.getElementById('canvas-element');
+var header = $('nav.navbar').offset().top;
 
-$(document).ready(function() {
+// $(document).ready(function() {
 
-    var header = $('nav.navbar').offset().top;
+//     $('a.link-home').click(function () {
+//         $.scrollTo('section.link-home');
+//     });
 
-    $('a.link-home').click(function () {
-        $.scrollTo('section.link-home');
-    });
+//     $('a.link-projects').click(function () {
+//         $.scrollTo('section.link-projects');
+//     });
 
-    $('a.link-projects').click(function () {
-        $.scrollTo('section.link-projects');
-    });
+//     $('a.link-aboutme').click(function () {
+//         $.scrollTo('section.link-aboutme');
+//     });
 
-    $('a.link-aboutme').click(function () {
-        $.scrollTo('section.link-aboutme');
-    });
+//     $('a.link-contact').click(function () {
+//         $.scrollTo('section.link-contact');
+//     });
+// });
 
-    $('a.link-contact').click(function () {
-        $.scrollTo('section.link-contact');
-    });
+
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
 });
