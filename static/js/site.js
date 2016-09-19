5b67a070d2378024eed7ba416dae8c90afe9a008
tdiede <tdiede@gmail.com>
2016-09-18 21:43:33 -0700

    Added favicon.

--- static/js/site.js
+++ static/js/site.js
@@ -4,26 +4,6 @@
 var canvas = document.getElementById('canvas-element');
 var header = $('nav.navbar').offset().top;
 
-// $(document).ready(function() {
-
-//     $('a.link-home').click(function () {
-//         $.scrollTo('section.link-home');
-//     });
-
-//     $('a.link-projects').click(function () {
-//         $.scrollTo('section.link-projects');
-//     });
-
-//     $('a.link-aboutme').click(function () {
-//         $.scrollTo('section.link-aboutme');
-//     });
-
-//     $('a.link-contact').click(function () {
-//         $.scrollTo('section.link-contact');
-//     });
-// });
-
-
 $('a[href^="#"]').on('click', function(event) {
     var target = $(this.getAttribute('href'));
     if( target.length ) {
