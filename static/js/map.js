74f5d3abb6c1170b1d65c7b0c8aac0db237bf8b4
Therese Diede <tdiede@gmail.com>
2016-10-08 01:39:37 +0000

    Added locations.geojson file.

--- static/js/map.js
+++ static/js/map.js
@@ -50,8 +50,8 @@ map.on('load', function () {
         "type": "symbol",
         "source": "points",
         "layout": {
-            "icon-image": "{icon}-15",
-            "text-field": "{title}",
+            "icon-image": "marker-15",
+            "text-field": "place",
             "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
             "text-offset": [0, 0.6],
             "text-anchor": "top"
