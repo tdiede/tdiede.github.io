029df4187cd35fed99ff1394b6f6fc70418d7a4a
Therese Diede <tdiede@gmail.com>
2016-10-11 00:24:10 +0000

    Updated jquery cdn.

--- static/js/site.js
+++ static/js/site.js
@@ -1,8 +1,8 @@
 "use strict";
 
-src="http://code.jquery.com/jquery-2.2.4.min.js"
-integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
-crossorigin="anonymous">
+src="http://code.jquery.com/jquery-2.2.4.min.js";
+integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=";
+crossorigin="anonymous";
 
 
 $(document).ready(function() {
