e47640a1ed1cafc2cda07797ffed55403aeaafa9
Therese Diede <tdiede@gmail.com>
2016-11-02 23:01:29 +0000

    Fixed jquery.

--- static/js/site.js
+++ static/js/site.js
@@ -47,8 +47,8 @@ function showFeature (result) {
 function submitProjectData (e) {
     e.preventDefault();
     console.log('clicnowww');
-    console.log(this.data;
-    var projectName = $(this.data());
+    console.log(this);
+    var projectName = $(this).data();
     var src = '/static/img/features/'+projectName+'.png';
     console.log(src);
     $.get(src, showFeature);
