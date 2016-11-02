959554e044b124fce323b5f91de1ef2a2336d1ec
Therese Diede <tdiede@gmail.com>
2016-11-02 23:05:35 +0000

    Fixed jquery.

--- static/js/site.js
+++ static/js/site.js
@@ -47,7 +47,7 @@ function showFeature (result) {
 function submitProjectData (e) {
     e.preventDefault();
     console.log(this);
-    console.log(this.href);
+    console.log(this.data);
     console.log('clicnowww');
     var projectName = $(this.getAttribute('data'));
     console.log(projectName);
