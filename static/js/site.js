52dd83bdbcc884700b29143d3db3cff36d6b28ea
Therese Diede <tdiede@gmail.com>
2016-11-02 21:53:06 +0000

    Reorganized photos in directory.

--- static/js/site.js
+++ static/js/site.js
@@ -46,7 +46,8 @@ function showFeature (result) {
 
 function submitProjectData (e) {
     e.preventDefault();
-    var projectName = $(this.data());
+    console.log('click');
+    var projectName = $(this.getAttribute('data'));
     var src = '/static/img/features/'+projectName+'.png';
     console.log(src);
     $.get(src, showFeature);
