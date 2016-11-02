7227fb6996b23a2911dc56bc2f08e4134ccde4cc
Therese Diede <tdiede@gmail.com>
2016-11-02 21:48:01 +0000

    Created variable data attribute to show feature project.

--- static/js/site.js
+++ static/js/site.js
@@ -54,4 +54,4 @@ function submitProjectData (e) {
     // $('#data-card-id').attr('data-card', result.card_id);
 }
 
-$('#project-photo-velib').on('submit', submitProjectData);
+$('.project-photo').on('submit', submitProjectData);
