32a4a4dc9d6164de80b4b73bcf1dda22b3a7bd71
Therese Diede <tdiede@gmail.com>
2016-11-03 20:31:16 +0000

    Reordering JS.

--- static/js/site.js
+++ static/js/site.js
@@ -11,31 +11,7 @@ $(document).ready(function() {
 // var canvas = document.getElementById('canvas-element');
 // var header = $('nav.navbar').offset().top;
 
-$('a[href^="#"]').on('click', function(e) {
-    var target = $(this.getAttribute('href'));
-    if ( target.length ) {
-        event.preventDefault();
-        $('html, body').stop().animate({
-            scrollTop: target.offset().top
-        }, 1000);
-    }
-});
 
-
-// $('.one').on('click', function(e) {
-//     $('.one').hide();
-//     $('.two').show();
-// });
-
-// $('.two').on('click', function(e) {
-//     $('.two').hide();
-//     $('.one').show();
-// });
-
-
-
-
-// var currentFeature;
 
 
 $('.project-link-one').on('click', function(e) {
@@ -67,3 +43,28 @@ $('.project-link-two').on('click', function(e) {
     $('#project-date-2').html(date);
     $('#project-description-2').html(description);
 });
+
+
+
+
+$('a[href^="#"]').on('click', function(e) {
+    var target = $(this.getAttribute('href'));
+    if ( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
+
+
+// $('.one').on('click', function(e) {
+//     $('.one').hide();
+//     $('.two').show();
+// });
+
+// $('.two').on('click', function(e) {
+//     $('.two').hide();
+//     $('.one').show();
+// });
+
