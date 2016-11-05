6b2b965391518a1b8efff398bc244dac199a7852
Therese Diede <tdiede@gmail.com>
2016-11-05 22:52:59 +0000

    Cleaning up json conversion work.

--- static/js/site.js
+++ static/js/site.js
@@ -77,8 +77,8 @@ $('a.project-link').on('click', function(e) {
     $('#project-title').html(title);
     $('#project-date-partners').html(date + " | " + partners);
     $('#project-description').html(description);
-    $('#project-techstack').html("<p>tech stack | " + techstack + "</p>");
-    $('#project-apis').html("<p>APIs | " + apis + "</p>");
+    $('#project-techstack').html("<p>tech stack | <h4>" + techstack + "</h4></p>");
+    $('#project-apis').html("<p>APIs | <h4>" + apis + "</h4></p>");
     $('#project-github').html("<p><span><i class='fa fa-github' aria-hidden='true'></i></span> review my code: <a href='" + github + "' target='_blank'>" + github + "</a></p>");
     $('#project-deployed').html("<p><span><i class='fa fa-user' aria-hidden='true'></i></span> try it out: <a href='" + deployed + "' target='_blank'>" + deployed + "</a></p>");
 });
